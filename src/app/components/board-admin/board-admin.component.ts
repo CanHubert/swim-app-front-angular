be80e054dfb4bed1ea4fb86cb83e12233580ee29
Hubert Can <canhubert@gmail.com>
2020-02-08 14:49:06 +0000

    stash test

--- src/app/components/board-admin/board-admin.component.ts
+++ src/app/components/board-admin/board-admin.component.ts
@@ -14,7 +14,11 @@ export class BoardAdminComponent implements OnInit {
     users: User[];
     dataSource: MatTableDataSource<User>;
     displayedColumns: string[] = ['position', 'weight', 'symbol', 'name'];
+<<<<<<< Updated upstream
     text = 'some text';
+=======
+
+>>>>>>> Stashed changes
   constructor(private userService: UserService) { }
 
   @ViewChild(MatPaginator , {static: false}) paginator: MatPaginator;
